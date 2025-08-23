03a696554b6060c284c94d0773705df6ed2a5737
gvsskumar <kumar.developer0701@gmail.com>
2025-08-23 07:11:49 +0530

    Python Data Types

---
+++ python/Basics/Datatypes.py
@@ -0,0 +1,229 @@
+#Fundamental Datatypes
+#1. String
+#2. Integer
+#3. Float
+#4. Boolean
+#5. List
+#6. Tuple
+#7. Dictionary
+#8. Set
+#9. None
+
+#1. String
+str1 = "Hello World"
+print(type(str1))
+
+
+#2. Integer
+num1 = 10
+print(type(num1))
+
+
+#3. Float
+num2 = 3.14
+print(type(num2))
+
+
+#4. Boolean
+bool1 = True
+print(type(bool1))
+
+
+#5. List
+list1 = [1, 2, 3, 4, 5]
+print(type(list1))
+
+
+#6. Tuple
+tuple1 = (1, 2, 3, 4, 5)
+print(type(tuple1))
+
+
+#7. Dictionary
+dict1 = {"name": "John", "age": 30}
+print(type(dict1))
+
+
+#8. Set
+set1 = {1, 2, 3, 4, 5}
+print(type(set1))
+
+
+#9. None
+none1 = None
+print(type(none1))
+
+#Output:
+#<class 'str'>
+#<class 'int'>
+#<class 'float'>
+#<class 'bool'>
+#<class 'list'>
+#<class 'tuple'>
+#<class 'dict'>
+#<class 'set'>
+#<class 'NoneType'>
+
+#Type Conversion
+#1. String to Integer
+str1 = "10"
+num1 = int(str1)
+print(type(num1))
+
+#Output:
+#<class 'int'>
+
+#2. Integer to String
+num1 = 10
+str1 = str(num1)
+print(type(str1))
+
+#Output:
+#<class 'str'>
+
+#3. String to Float
+str1 = "3.14"
+num1 = float(str1)
+print(type(num1))
+
+#Output:    
+#<class 'float'>
+
+#4. Float to String
+num1 = 3.14
+str1 = str(num1)
+print(type(str1))
+
+#Output:
+#<class 'str'>
+
+#5. String to Boolean
+str1 = "True"
+bool1 = bool(str1)
+print(type(bool1))
+
+#Output:
+#<class 'bool'>
+
+#6. Boolean to String
+bool1 = True
+str1 = str(bool1)
+print(type(str1))
+
+#Output:
+#<class 'str'>
+
+#7. Integer to Boolean
+num1 = 1
+bool1 = bool(num1)
+print(type(bool1))
+
+#Output:
+#<class 'bool'>
+
+#8. Boolean to Integer
+bool1 = True
+num1 = int(bool1)
+print(type(num1))
+
+#Output:
+#<class 'int'>
+
+#9. Float to Integer
+num1 = 3.14
+num2 = int(num1)
+print(type(num2))
+
+#Output:
+#<class 'int'>
+
+#10. Integer to Float
+num1 = 10
+num2 = float(num1)
+print(type(num2))
+
+#Output:
+#<class 'float'>
+
+#11. Boolean to Float
+# There is no conversion from Boolean to Float
+# You can't convert Boolean to Float
+
+#12. Float to Boolean
+# There is no conversion from Float to Boolean
+# You can't convert Float to Boolean
+
+#13. String to Boolean
+# There is no conversion from String to Boolean
+# You can't convert String to Boolean
+
+#14. Boolean to String
+# There is no conversion from Boolean to String
+# You can't convert Boolean to String
+
+#15. Integer to String
+# There is no conversion from Integer to String
+# You can't convert Integer to String
+
+#16. String to Integer
+# There is no conversion from String to Integer
+# You can't convert String to Integer
+
+#17. Float to String
+# There is no conversion from Float to String
+# You can't convert Float to String
+
+#18. String to Float
+# There is no conversion from String to Float
+# You can't convert String to Float
+
+#19. Integer to Float
+# There is no conversion from Integer to Float
+# You can't convert Integer to Float
+
+#20. Float to Integer
+# There is no conversion from Float to Integer
+# You can't convert Float to Integer
+
+#special datatypes
+# 1. None
+# 2. Ellipsis
+# 3. NotImplemented
+# 4. NotImplementedType
+# 5. NotImplementedClass
+# 6. NotImplementedInstance
+# 7. NotImplementedSubclass
+# 8. NotImplementedSubclassInstance
+
+#None is used to represent an empty value or a null value in a variable. It is often used to indicate that a variable has not been assigned a value yet.
+
+#Ellipsis is used to represent a placeholder in a sequence or a slice. It is often used to indicate that a sequence or slice is empty.
+
+#NotImplemented is used to represent a method that has not been implemented yet. It is often used to indicate that a method has not been implemented yet.
+
+#NotImplementedType is used to represent a type that has not been implemented yet. It is often used to indicate that a type has not been implemented yet.
+
+#NotImplementedClass is used to represent a class that has not been implemented yet. It is often used to indicate that a class has not been implemented yet.
+
+#NotImplementedInstance is used to represent an instance of a class that has not been implemented yet. It is often used to indicate that an instance of a class has not been implemented yet.      
+
+#NotImplementedSubclass is used to represent a subclass of a class that has not been implemented yet. It is often used to indicate that a subclass of a class has not been implemented yet.
+
+#Custom datatypes
+#1. User-defined classes
+#2. User-defined functions
+#3. User-defined modules
+#4. User-defined exceptions
+
+#User-defined classes are user-defined data types that are created by the user. They are defined using the class keyword.
+
+#User-defined functions are user-defined functions that are created by the user. They are defined using the def keyword.
+
+#User-defined modules are user-defined modules that are created by the user. They are defined using the import keyword.
+
+#User-defined exceptions are user-defined exceptions that are created by the user. They are defined using the raise keyword.
+
+
+
+
+
