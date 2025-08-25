0ac7123823bc27a965534f318c0add530a7ba0ef
gvsskumar <kumar.developer0701@gmail.com>
2025-08-25 21:03:52 +0530

    Variables

---
+++ python/Basics/Variables.py
@@ -0,0 +1,19 @@
+#Variables
+a = 10
+b = 3.14
+c = "Hello"
+d = True
+e = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#Multiple Assignment
+x, y, z = 1, 2, 3
+print(x, y, z)  # 1 2 3
+
+a = b = c = 0   # all assigned to 0
+
